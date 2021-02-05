c354f064be1f592ad4f0f2a290050a67d701154b
Ruaraidh <rca.plummer@gmail.com>
2021-02-05 13:45:44 +0000

    #3 - variables added to .js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const start = document.querySelector(".start-button");
+const roll = document.querySelector(".roll-button");
+
+const dice = document.querySelector(".dice-image");
+
+const title = document.querySelector(".header");
+const score = document.querySelector(".rolled-number");
 
